e0e9be6030010b3bf5a10c67ff5f4f24b7b693bd
RajPraneethChavvakula <163905759+RajPraneethChavvakula@users.noreply.github.com>
2024-11-27 01:49:23 -0500

    Create src/data/data_loader.py

---
+++ src/data/data_loader.py
@@ -0,0 +1,12 @@
+
+from datasets import load_dataset
+
+def load_simplewiki_data():
+    """Load and preprocess SimpleWiki dataset"""
+    # TODO: Implement dataset loading
+    pass
+
+def load_access_dataset():
+    """Load and preprocess ACCESS dataset"""
+    # TODO: Implement dataset loading
+    pass
