c04161f59d2848505d175da1db449a9df9e6db1a
RajPraneethChavvakula <163905759+RajPraneethChavvakula@users.noreply.github.com>
2024-11-27 01:49:35 -0500

    Create src/utils/metrics.py

---
+++ src/utils/metrics.py
@@ -0,0 +1,10 @@
+
+def calculate_sari(original, simplified, references):
+    """Calculate SARI score"""
+    # TODO: Implement SARI calculation
+    pass
+
+def calculate_bleu(original, simplified):
+    """Calculate BLEU score"""
+    # TODO: Implement BLEU calculation
+    pass
