33939c43704aa8f97dd1988252aafbd6d0be06ce
RajPraneethChavvakula <163905759+RajPraneethChavvakula@users.noreply.github.com>
2024-11-27 01:49:29 -0500

    Create src/models/simplification_model.py

---
+++ src/models/simplification_model.py
@@ -0,0 +1,14 @@
+
+import torch
+import torch.nn as nn
+
+class TextSimplificationModel(nn.Module):
+    """Main text simplification model"""
+    
+    def __init__(self):
+        super().__init__()
+        # TODO: Implement model architecture
+        
+    def forward(self, x):
+        # TODO: Implement forward pass
+        pass
