6be4aaedb94d84664f9118447059aa9a2e01a581
RajPraneethChavvakula <163905759+RajPraneethChavvakula@users.noreply.github.com>
2024-11-27 01:49:25 -0500

    Create src/data/preprocessor.py

---
+++ src/data/preprocessor.py
@@ -0,0 +1,16 @@
+
+class TextPreprocessor:
+    """Handle text preprocessing operations"""
+    
+    def __init__(self):
+        pass
+        
+    def clean_text(self, text):
+        """Clean and normalize text"""
+        # TODO: Implement text cleaning
+        pass
+        
+    def tokenize(self, text):
+        """Tokenize text"""
+        # TODO: Implement tokenization
+        pass
