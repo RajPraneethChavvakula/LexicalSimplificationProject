49e5ca20ca202145739ed6c397aa65db99e3baf3
RajPraneethChavvakula <163905759+RajPraneethChavvakula@users.noreply.github.com>
2024-11-27 01:49:40 -0500

    Create tests/test_simplification.py

---
+++ tests/test_simplification.py
@@ -0,0 +1,11 @@
+
+import unittest
+
+class TestSimplification(unittest.TestCase):
+    def test_sentence_splitting(self):
+        # TODO: Implement test cases
+        pass
+        
+    def test_complex_word_identification(self):
+        # TODO: Implement test cases
+        pass
